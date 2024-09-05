3c81c158e45ebe1f72fb0a14e6220deb61ae96ae
faruq-dev <faruq2109@gmail.com>
2024-09-05 19:45:52 +0600

    Backgroundd Images and Weather Icons changes Applied

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const cityName = document.getElementById('city-name');
 const currentTemp = document.getElementById('temp');
 const currentDateTime = document.getElementById('time-date');
+const weatherIcon = document.getElementById('weather-icon');
+const bgImage = document.getElementById('bg-image')
 
 
 const API_KEY = '5cf4a02746088171dd0cf1df5d1db703';
@@ -24,10 +26,49 @@ const getWeather = async (lat, lon)=>{
         currentTemp.style.color = 'white'
     };
 
-    
+    switch(data?.weather[0]?.main){
+        case 'Haze':
+            weatherIcon.innerHTML = `<i class="fa-solid fa-smog"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/few-clouds.jpg')`;
+            break;
+
+        case 'Clear':
+            weatherIcon.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/clear-sky.jpg')`;
+            break;
+
+        case 'Rain':
+            weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/shower-rain.jpg')`;
+            break;
+
+        case 'Thunderstorm':
+            weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-bolt"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/thunderstorm-2.jpg')`;
+            break;
+
+        case 'Clouds':
+            weatherIcon.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/clouds.jpg')`;
+            break;
+
+        case 'Fog':
+            weatherIcon.innerHTML = `<i class="bi bi-cloud-fog2-fill"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/mist.jpg')`;
+            break;
+            
+        case 'Snow':
+            weatherIcon.innerHTML = `<i class="bi bi-cloud-snow-fill"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/snow.jpg')`;
+            break;
+
+        default :
+            weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-sun-rain"></i>`;
+            bgImage.style.backgroundImage = `url('/assets/backgrounds/honza-reznik-tUsIUWNEtsQ-unsplash.jpg')`;
+
+    }
+
 };
-
-
 
 
 navigator.geolocation.getCurrentPosition((position)=>{
